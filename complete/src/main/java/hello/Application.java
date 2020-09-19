4355f4cd980427535517e7f8e0114ab50eea3e3b
kofiandoh <andoh.samuel@gmail.com>
2020-09-19 00:28:19 -0400

    Update Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "I am trying to conteainerize my doceker application";
 	}
 
 	public static void main(String[] args) {
